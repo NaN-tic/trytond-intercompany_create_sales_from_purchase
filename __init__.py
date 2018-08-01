a3f67f72a43b3ca2f81eaddd884f417c18a0a676
?ngel ?lvarez <angel@nan-tic.com>
2018-08-01 16:05:52 +0200

    Use user company defined to create new sales

--- __init__.py
+++ __init__.py
@@ -3,10 +3,9 @@
 from trytond.pool import Pool
 from . import purchase
 
-__all__ = ['Purchase']
-
 
 def register():
     Pool.register(
         purchase.Purchase,
+        purchase.Company,
         module='intercompany_create_sales_from_purchase', type_='model')
