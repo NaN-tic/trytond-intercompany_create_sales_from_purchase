daac1b66c8c45d354228303ae4b1504b65e80ad1
Raimon Esteve <raimon@nan-tic.com>
2018-09-03 12:46:59 +0200

    FIX process purchases from confirmed and set unit price from purchase line in case product has not a price task-035329

--- __init__.py
+++ __init__.py
@@ -2,10 +2,11 @@
 # copyright notices and license terms.
 from trytond.pool import Pool
 from . import purchase
-
+from . import sale
 
 def register():
     Pool.register(
         purchase.Purchase,
         purchase.Company,
+        sale.SaleLine,
         module='intercompany_create_sales_from_purchase', type_='model')
