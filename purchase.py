962dd69976806ca1406ceb93df84ac9cb4b905d8
Raimon Esteve <raimon@nan-tic.com>
2018-09-03 17:14:21 +0200

    Copy purchase line price in case product has not a unit price

--- purchase.py
+++ purchase.py
@@ -111,7 +111,7 @@ class Purchase:
         sale_line.quantity = line.quantity
         sale_line.on_change_product()
         if not sale_line.unit_price:
-            sale_line.unit_price = Decimal(0.0)
+            sale_line.unit_price = line.unit_price
         if hasattr(sale_line, 'gross_unit_price'):
             sale_line.gross_unit_price = sale_line.unit_price
         sale_line.purchase_line = line
