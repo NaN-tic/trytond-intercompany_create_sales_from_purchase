aa6aa720456a9c906923788e70a060dd6799a5e4
Raimon Esteve <raimon@nan-tic.com>
2018-12-19 15:40:49 +0100

    Set gross unit price in case has not unit_price and quantize gross unit digits #036352

--- purchase.py
+++ purchase.py
@@ -115,7 +115,8 @@ class Purchase(metaclass=PoolMeta):
         sale_line.on_change_product()
         if not sale_line.unit_price:
             sale_line.unit_price = line.unit_price
-        if hasattr(sale_line, 'gross_unit_price'):
-            sale_line.gross_unit_price = sale_line.unit_price
+            if hasattr(SaleLine, 'gross_unit_price'):
+                sale_line.gross_unit_price = sale_line.unit_price.quantize(
+                    Decimal(1) / 10 ** SaleLine.gross_unit_price.digits[1])
         sale_line.purchase_line = line
         return sale_line
